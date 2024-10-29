6114d28ce8259454d62729d679940b3d9bcffaf0
Pedro Júnior <pedrojdev@gmail.com>
2024-10-29 10:54:20 -0300

    feat: make `Surface` control public

--- src/WPF.Material/Controls/Surface.cs
+++ src/WPF.Material/Controls/Surface.cs
@@ -2,111 +2,174 @@
 
 namespace WPF.Material.Controls;
 
-internal sealed class Surface : FrameworkElement
+/// <summary>
+/// Represents a control used to render a surface with given shape attributes.
+/// </summary>
+public class Surface : FrameworkElement
 {
+    /// <summary>
+    /// Identifies the <see cref="Background"/> dependency property.
+    /// </summary>
     public static readonly DependencyProperty BackgroundProperty = Control.BackgroundProperty.AddOwner(
         typeof(Surface),
         new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
 
+    /// <summary>
+    /// Identifies the <see cref="BorderBrush"/> dependency property.
+    /// </summary>
     public static readonly DependencyProperty BorderBrushProperty = Control.BorderBrushProperty.AddOwner(
         typeof(Surface),
         new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
 
+    /// <summary>
+    /// Identifies the <see cref="BorderThickness"/> dependency property.
+    /// </summary>
     public static readonly DependencyProperty BorderThicknessProperty = Control.BorderThicknessProperty.AddOwner(
         typeof(Surface),
         new FrameworkPropertyMetadata(new Thickness(), FrameworkPropertyMetadataOptions.AffectsRender));
 
+    /// <summary>
+    /// Identifies the <see cref="ShapeFamily"/> dependency property.
+    /// </summary>
     public static readonly DependencyProperty ShapeFamilyProperty = DependencyProperty.Register(
         nameof(ShapeFamily),
         typeof(ShapeFamily),
         typeof(Surface),
         new FrameworkPropertyMetadata(ShapeFamily.Rounded, FrameworkPropertyMetadataOptions.AffectsRender));
 
+    /// <summary>
+    /// Identifies the <see cref="ShapeStyle"/> dependency property.
+    /// </summary>
     public static readonly DependencyProperty ShapeStyleProperty = DependencyProperty.Register(
         nameof(ShapeStyle),
         typeof(ShapeStyle),
         typeof(Surface),
         new FrameworkPropertyMetadata(ShapeStyle.None, FrameworkPropertyMetadataOptions.AffectsRender));
 
+    /// <summary>
+    /// Identifies the <see cref="ShapeCorner"/> dependency property.
+    /// </summary>
     public static readonly DependencyProperty ShapeCornerProperty = DependencyProperty.Register(
         nameof(ShapeCorner),
         typeof(ShapeCorner),
         typeof(Surface),
         new FrameworkPropertyMetadata(ShapeCorner.All, FrameworkPropertyMetadataOptions.AffectsRender));
 
+    /// <summary>
+    /// Identifies the <see cref="ShapeRadius"/> dependency property.
+    /// </summary>
     public static readonly DependencyProperty ShapeRadiusProperty = DependencyProperty.Register(
         nameof(ShapeRadius),
         typeof(CornerRadius?),
         typeof(Surface),
         new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
-
+    
     private static readonly DependencyPropertyKey RenderedGeometryPropertyKey = DependencyProperty.RegisterReadOnly(
         nameof(RenderedGeometry),
         typeof(Geometry),
         typeof(Surface),
         new PropertyMetadata(null));
 
+    /// <summary>
+    /// Identifies the <see cref="RenderedGeometry"/> dependency property.
+    /// </summary>
     public static readonly DependencyProperty RenderedGeometryProperty = RenderedGeometryPropertyKey.DependencyProperty;
     
+    /// <summary>
+    /// Identifies the <see cref="Rendered"/> routed event.
+    /// </summary>
     public static readonly RoutedEvent RenderedEvent  = EventManager.RegisterRoutedEvent(
         nameof(Rendered),
         RoutingStrategy.Bubble, 
         typeof(RoutedEventHandler),
         typeof(Surface)); 
     
+    /// <summary>
+    /// Gets or sets the <see cref="Brush"/> used to draw the background of the control.
+    /// </summary>
     public Brush? Background
     {
         get => (Brush)GetValue(BackgroundProperty);
         set => SetValue(BackgroundProperty, value);
     }
 
+    /// <summary>
+    /// Gets or sets the <see cref="Brush"/> used to draw the border of the control.
+    /// </summary>
     public Brush? BorderBrush
     {
         get => (Brush)GetValue(BorderBrushProperty);
         set => SetValue(BorderBrushProperty, value);
     }
 
+    /// <summary>
+    /// Gets or sets the <see cref="Thickness"/> of the border around the control.
+    /// </summary>
     public Thickness BorderThickness
     {
         get => (Thickness)GetValue(BorderThicknessProperty);
         set => SetValue(BorderThicknessProperty, value);
     }
 
+    /// <summary>
+    /// Gets or sets the family of the shape to render.
+    /// </summary>
     public ShapeFamily ShapeFamily
     {
         get => (ShapeFamily)GetValue(ShapeFamilyProperty);
         set => SetValue(ShapeFamilyProperty, value);
     }
 
+    /// <summary>
+    /// Gets or sets the style of the shape, which determines the shape's appearance.
+    /// </summary>
     public ShapeStyle ShapeStyle
     {
         get => (ShapeStyle)GetValue(ShapeStyleProperty);
         set => SetValue(ShapeStyleProperty, value);
     }
 
+    /// <summary>
+    /// Gets or sets the corner that determines which ones should be considered on rendering.
+    /// </summary>
     public ShapeCorner ShapeCorner
     {
         get => (ShapeCorner)GetValue(ShapeCornerProperty);
         set => SetValue(ShapeCornerProperty, value);
     }
 
+    /// <summary>
+    /// Gets or sets the radius to override the default one from the style.
+    /// </summary>
     public CornerRadius? ShapeRadius
     {
         get => (CornerRadius?)GetValue(ShapeRadiusProperty);
         set => SetValue(ShapeRadiusProperty, value);
     }
 
+    /// <summary>
+    /// Gets the rendered geometry of the surface based on the shape attributes.
+    /// </summary>
     public Geometry? RenderedGeometry
     {
         get => (Geometry?)GetValue(RenderedGeometryProperty);
         private set => SetValue(RenderedGeometryPropertyKey, value);
     }
     
+    /// <summary>
+    /// Occurs when the surface has been rendered.
+    /// </summary>
     public event RoutedEventHandler Rendered
     {
         add => AddHandler(RenderedEvent, value);
         remove => RemoveHandler(RenderedEvent, value);
     }
+    
+    /// <summary>
+    /// Occurs when the surface has been rendered.
+    /// </summary>
+    /// <param name="e">The event data.</param>
+    protected virtual void OnRendered(RoutedEventArgs e) => RaiseEvent(e);
 
     protected override void OnRender(DrawingContext context)
     {
@@ -139,7 +202,7 @@ internal sealed class Surface : FrameworkElement
         context.DrawGeometry(Background, pen, geometry);
 
         RenderedGeometry = geometry;
-        RaiseEvent(new RoutedEventArgs(RenderedEvent));
+        OnRendered(new RoutedEventArgs(RenderedEvent));
     }
     
     private static StreamGeometry CreateGeometry(ShapeFamily family, Rect bounds, CornerRadius radius, bool isStroked)
