20d0c1631230e4bdc1ee7a0c11a28cc02d7ecc45
Pedro Júnior <pedrojdev@gmail.com>
2024-10-31 13:28:16 -0300

    feat: support horizontal and vertical children stretching in `SpacedPanel`

--- src/WPF.Material/Controls/SpacedPanel.cs
+++ src/WPF.Material/Controls/SpacedPanel.cs
@@ -51,7 +51,33 @@ public class SpacedPanel : Panel
         new FrameworkPropertyMetadata(
             1.0,
             FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsMeasure));
-    
+
+    /// <summary>
+    /// Identifies the StretchHorizontally dependency property.
+    /// </summary>
+    public static readonly DependencyProperty StretchHorizontallyProperty = DependencyProperty.RegisterAttached(
+        "StretchHorizontally",
+        typeof(bool),
+        typeof(SpacedPanel),
+        new FrameworkPropertyMetadata(
+            false,
+            FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+    /// <summary>
+    /// Identifies the StretchVertically dependency property.
+    /// </summary>
+    public static readonly DependencyProperty StretchVerticallyProperty = DependencyProperty.RegisterAttached(
+        "StretchVertically",
+        typeof(bool),
+        typeof(SpacedPanel),
+        new FrameworkPropertyMetadata(
+            false,
+            FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+    private int stretchableChildrenCount;
+    private double totalNonStretchedWidth;
+    private double totalNonStretchedHeight;
+
     /// <summary>
     /// Initializes static members of the <see cref="SpacedPanel"/> class.
     /// </summary>
@@ -96,7 +122,7 @@ public class SpacedPanel : Panel
         get => (bool)GetValue(JoinItemBordersProperty);
         set => SetValue(JoinItemBordersProperty, value);
     }
-    
+
     /// <summary>
     /// Gets or sets the uniform thickness of the border around each item. This value is used when the items are
     /// eligible for joining borders. This property doesn't set the border thickness of the items, but it is used to
@@ -109,99 +135,216 @@ public class SpacedPanel : Panel
         get => (double)GetValue(ItemBorderThicknessProperty);
         set => SetValue(ItemBorderThicknessProperty, value);
     }
+    
+        /// <summary>
+    /// Sets the value of the <see cref="StretchHorizontallyProperty"/> attached property for a specified dependency object.
+    /// </summary>
+    /// <param name="element">
+    /// The dependency object for which to set the value of the <see cref="StretchHorizontallyProperty"/> property.
+    /// </param>
+    /// <param name="value">The new value to set the property to.</param>
+    public static void SetStretchHorizontally(DependencyObject element, bool value) =>
+        element.SetValue(StretchHorizontallyProperty, value);
 
-    protected override Size MeasureOverride(Size constraints)
+    /// <summary>
+    /// Gets the value of the <see cref="StretchHorizontallyProperty"/> attached property for a specified dependency object.
+    /// </summary>
+    /// <param name="element">
+    /// The dependency object for which to retrieve the value of the <see cref="StretchHorizontallyProperty"/> property.
+    /// </param>
+    /// <returns>
+    /// The current value of the <see cref="StretchHorizontallyProperty"/> attached property on the specified dependency object.
+    /// </returns>
+    public static bool GetStretchHorizontally(DependencyObject element) =>
+        (bool)element.GetValue(StretchHorizontallyProperty);
+
+    /// <summary>
+    /// Sets the value of the <see cref="StretchVerticallyProperty"/> attached property for a specified dependency object.
+    /// </summary>
+    /// <param name="element">
+    /// The dependency object for which to set the value of the <see cref="StretchVerticallyProperty"/> property.
+    /// </param>
+    /// <param name="value">The new value to set the property to.</param>
+    public static void SetStretchVertically(DependencyObject element, bool value) =>
+        element.SetValue(StretchVerticallyProperty, value);
+
+    /// <summary>
+    /// Gets the value of the <see cref="StretchVerticallyProperty"/> attached property for a specified dependency object.
+    /// </summary>
+    /// <param name="element">
+    /// The dependency object for which to retrieve the value of the <see cref="StretchVerticallyProperty"/> property.
+    /// </param>
+    /// <returns>
+    /// The current value of the <see cref="StretchVerticallyProperty"/> attached property on the specified dependency object.
+    /// </returns>
+    public static bool GetStretchVertically(DependencyObject element) =>
+        (bool)element.GetValue(StretchVerticallyProperty);
+
+    protected override Size MeasureOverride(Size availableSize)
     {
         var children = Children;
         var spacing = Spacing;
         var orientation = Orientation;
+        var joinItemBorders = JoinItemBorders && spacing is 0.0;
+        var itemBorderThickness = ItemBorderThickness;
 
         if (children.Count is 0)
         {
             return Size.Empty;
         }
 
-        var width = 0.0;
-        var height = 0.0;
+        var totalWidth = 0.0;
+        var totalHeight = 0.0;
+
+        stretchableChildrenCount = 0;
+        totalNonStretchedWidth = 0.0;
+        totalNonStretchedHeight = 0.0;
 
         for (var i = 0; i < children.Count; i++)
         {
             var child = children[i];
 
-            child.Measure(constraints);
-
+            // Let the child measure itself.
+            child.Measure(availableSize);
             var childSize = child.DesiredSize;
 
+            // Calculate the total width and height of the panel based on the orientation and the size of the child.
             if (orientation is Orientation.Horizontal)
             {
-                width += childSize.Width;
-                height = Math.Max(height, childSize.Height);
+                totalWidth += childSize.Width;
+                totalHeight = Math.Max(totalHeight, childSize.Height);
             }
             else
             {
-                height += childSize.Height;
-                width = Math.Max(width, childSize.Width);
+                totalHeight += childSize.Height;
+                totalWidth = Math.Max(totalWidth, childSize.Width);
+            }
+
+            // Count the number of stretchable children and the total width and height of the non-stretched children.
+            if (GetStretchHorizontally(child) || GetStretchVertically(child))
+            {
+                stretchableChildrenCount++;
+            }
+            else
+            {
+                if (orientation is Orientation.Horizontal)
+                {
+                    totalNonStretchedWidth += childSize.Width;
+                }
+                else
+                {
+                    totalNonStretchedHeight += childSize.Height;
+                }
             }
         }
 
-        // Space between children
-        var spacingBetween = spacing * (children.Count - 1);
+        // Adjust the total size of the panel by adding the total spacing between children and removing the total
+        // border thickness that has been joined. Also, ensures that the total size is at least the size of the
+        // available space if there are any stretchable children.
+
+        var totalSpacing = spacing * (children.Count - 1);
+        var totalJoinedBorderThickness = joinItemBorders
+            ? itemBorderThickness * (children.Count - 1)
+            : 0.0;
+
         if (orientation is Orientation.Horizontal)
         {
-            width += spacingBetween;
+            totalWidth += totalSpacing - totalJoinedBorderThickness;
+            if (stretchableChildrenCount > 0)
+            {
+                totalWidth = Math.Max(totalWidth, availableSize.Width);
+                totalNonStretchedWidth -= totalJoinedBorderThickness;
+            }
         }
         else
         {
-            height += spacingBetween;
+            totalHeight += totalSpacing - totalJoinedBorderThickness;
+            if (stretchableChildrenCount > 0)
+            {
+                totalHeight = Math.Max(totalHeight, availableSize.Height);
+                totalNonStretchedHeight -= totalJoinedBorderThickness;
+            }
         }
 
-        return new Size(width, height);
+        return new Size(totalWidth, totalHeight);
     }
 
-    protected override Size ArrangeOverride(Size constraints)
+    protected override Size ArrangeOverride(Size finalSize)
     {
         var children = Children;
         var spacing = Math.Abs(Spacing);
         var orientation = Orientation;
         var joinItemBorders = JoinItemBorders && spacing is 0.0;
-        var uniformItemBorderThickness = ItemBorderThickness;
+        var itemBorderThickness = ItemBorderThickness;
 
         if (children.Count is 0)
         {
-            return constraints;
+            return finalSize;
         }
 
-        // Offset of the next child
+        // The offset of the next child.
         var offsetX = 0.0;
         var offsetY = 0.0;
+
+        var totalSpacing = spacing * (children.Count - 1);
+
+        // Calculate the remaining width and height after arranging the non-stretched children.
+        var remainingWidth = finalSize.Width - totalNonStretchedWidth - totalSpacing;
+        var remainingHeight = finalSize.Height - totalNonStretchedHeight - totalSpacing;
+
+        // Calculate the width and height of the stretchable children.
+        var stretchableChildWidth = stretchableChildrenCount > 0 ? remainingWidth / stretchableChildrenCount : 0.0;
+        var stretchableChildHeight = stretchableChildrenCount > 0 ? remainingHeight / stretchableChildrenCount : 0.0;
 
         for (var i = 0; i < children.Count; i++)
         {
             var child = children[i];
             var childSize = child.DesiredSize;
-
+            
             if (orientation is Orientation.Horizontal)
             {
-                child.Arrange(new Rect(offsetX, 0.0, childSize.Width, constraints.Height));
-                offsetX += childSize.Width + spacing;
+                var childWidth = childSize.Width;
 
+                // Use the stretchable width if the child is stretchable.
+                if (GetStretchHorizontally(child))
+                {
+                    childWidth = stretchableChildWidth;
+                }
+                
+                child.Arrange(new Rect(offsetX, 0.0, childWidth, finalSize.Height));
+
+                // Update the offset for the next child.
+                offsetX += Math.Round(childWidth + spacing);
+
+                // Adjust the offset to join the borders of the items.
                 if (joinItemBorders && i < children.Count - 1)
                 {
-                    offsetX -= uniformItemBorderThickness;
+                    offsetX = Math.Round(offsetX) - itemBorderThickness;
                 }
             }
             else
             {
-                child.Arrange(new Rect(0.0, offsetY, constraints.Width, childSize.Height));
-                offsetY += childSize.Height + spacing;
+                var childHeight = childSize.Height;
 
+                // Use the stretchable height if the child is stretchable.
+                if (GetStretchVertically(child))
+                {
+                    childHeight = stretchableChildHeight;
+                }
+
+                child.Arrange(new Rect(0.0, offsetY, finalSize.Width, childHeight));
+
+                // Update the offset for the next child.
+                offsetY += Math.Round(childHeight + spacing);
+
+                // Adjust the offset to join the borders of the items.
                 if (joinItemBorders && i < children.Count - 1)
                 {
-                    offsetY -= uniformItemBorderThickness;
+                    offsetY = Math.Round(offsetY) - itemBorderThickness;
                 }
             }
         }
 
-        return constraints;
+        return finalSize;
     }
 }
